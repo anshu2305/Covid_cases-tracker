b87d04c692f7bf1fc44f7920f32a8109e8a5e3d2
Anshika Sariya <56234295+anshu2305@users.noreply.github.com>
2020-08-19 06:16:42 +0530

    Update world.js

--- world.js
+++ world.js
@@ -1,14 +1,15 @@
-/*fetch("https://api.covid19data.cloud/openapi.json")*/
-fetch("https://api.covid19api.com/summary")
-.then((response)=>{
-    return response.json();
-})
-.then((data)=>{
-    document.getElementById("total").innerHTML=data.Global.TotalConfirmed.toLocaleString();
-    document.getElementById("recovered").innerHTML=data.Global.TotalRecovered.toLocaleString();
-    document.getElementById("death").innerHTML=data.Global.TotalDeaths.toLocaleString();
-    document.getElementById("todaycases").innerHTML=data.Global.NewConfirmed;
-    document.getElementById("todaydeaths").innerHTML=data.Global.NewDeaths;
-    document.getElementById("todayrec").innerHTML=data.Global.NewRecovered;
-    document.getElementById("time").innerHTML=data.Date;
-})
+setInterval(() => {
+    fetch("https://api.covid19api.com/summary")
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        document.getElementById("total").innerHTML=data.Global.TotalConfirmed.toLocaleString();
+        document.getElementById("recovered").innerHTML=data.Global.TotalRecovered.toLocaleString();
+        document.getElementById("death").innerHTML=data.Global.TotalDeaths.toLocaleString();
+        document.getElementById("todaycases").innerHTML=data.Global.NewConfirmed;
+        document.getElementById("todaydeaths").innerHTML=data.Global.NewDeaths;
+        document.getElementById("todayrec").innerHTML=data.Global.NewRecovered;
+        document.getElementById("time").innerHTML=data.Date;
+    })
+}, 60000);    
